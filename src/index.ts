8afe164a2682eaf1aad369e7998ec9f12c817fb9
wild-mouse <wildmouse0813@gmail.com>
2021-02-04 11:16:48 +0900

    Add exporting interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 export * from "./balance-tree"
+export * from "./string-balance-tree"
 export * from "./merkle-tree"
-export { parseBalanceMap, OldFormat, NewFormat, MerkleDistributorInfo } from "./parse-balance-map"
+export {
+    parseBalanceMap,
+    OldFormat as BalanceMapOldFormat,
+    NewFormat as BalanceMapNewFormat,
+    MerkleDistributorInfo
+} from "./parse-balance-map"
+export {
+    parseStringBalanceMap,
+    OldFormat as StringBalanceMapOldFormat,
+    NewFormat as StringBalanceMapNewFormat,
+    MerkleDistributorInfo as StringMerkleDistributorInfo
+} from "./parse-string-balance-map"
