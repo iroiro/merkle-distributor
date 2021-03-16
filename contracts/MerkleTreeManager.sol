00a4ea7659acfeb9af730f3592cd54154f625631
wild-mouse <wildmouse0813@gmail.com>
2021-03-16 22:06:20 +0900

    Add tree managers

---
+++ contracts/MerkleTreeManager.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.7.6;
+
+import "@openzeppelin/contracts/cryptography/MerkleProof.sol";
+import "./interfaces/IMerkleTreeManager.sol";
+
+contract MerkleTreeManager is IMerkleTreeManager {
+    function proof(
+        uint64 treeId,
+        uint256 index,
+        address account,
+        uint256 amount,
+        bytes32[] calldata merkleProof
+    ) virtual external returns(bool) {
+        require(!isProven(treeId, index), 'MerkleTree: Already proven.');
+        bytes32 merkleRoot = merkleRootMap[treeId];
+
+        // Verify the merkle proof.
+        bytes32 node = keccak256(abi.encodePacked(index, account, amount));
+        require(MerkleProof.verify(merkleProof, merkleRoot, node), 'MerkleTree: Invalid proof.');
+
+        // Mark it claimed and send the token.
+        _setProven(treeId, index);
+
+        return true;
+    }
+}
